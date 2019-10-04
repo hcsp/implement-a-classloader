70b3189baef03c4e8adbad564a0f809380651234
JayChenFE <jaychenfe@foxmail.com>
2019-10-04 16:06:03 +0800

    实现ClassLoader (#3)
    实现ClassLoader (#3)

--- src/main/java/com/github/hcsp/classloader/MyClassLoader.java
+++ src/main/java/com/github/hcsp/classloader/MyClassLoader.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.classloader;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class MyClassLoader extends ClassLoader {
     // 存放字节码文件的目录
@@ -25,7 +27,21 @@ public class MyClassLoader extends ClassLoader {
     // 扩展阅读：ClassLoader类的Javadoc文档
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        throw new ClassNotFoundException(name);
+        Class<?> c = findLoadedClass(name);
+        if (c != null) {
+            return c;
+        }
+
+        try {
+            byte[] byteData = getByteData(name);
+            return defineClass(name, byteData, 0, byteData.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException();
+        }
+    }
+
+    private byte[] getByteData(String className) throws IOException {
+        return Files.readAllBytes(new File(bytecodeFileDirectory, className + ".class").toPath());
     }
 
     public static void main(String[] args) throws Exception {
