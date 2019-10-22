2d1a135371bb3b689077abad558846f5cdd7aab1
doubeans <sean.yang.shen@gmail.com>
2019-10-22 22:58:23 +0800

    debug

--- src/main/java/com/github/hcsp/classloader/MyClassLoader.java
+++ src/main/java/com/github/hcsp/classloader/MyClassLoader.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.classloader;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class MyClassLoader extends ClassLoader {
     // 存放字节码文件的目录
@@ -25,7 +27,16 @@ public class MyClassLoader extends ClassLoader {
     // 扩展阅读：ClassLoader类的Javadoc文档
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        throw new ClassNotFoundException(name);
+        try {
+            File file = new File(bytecodeFileDirectory, name + ".class");
+            FileInputStream inputStream = new FileInputStream(file);
+            byte[] bytes = new byte[1024];
+            int read = inputStream.read(bytes);
+            return defineClass(name, bytes, 0, read);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new ClassNotFoundException(name);
+        }
     }
 
     public static void main(String[] args) throws Exception {
