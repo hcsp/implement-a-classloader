d36da1b5fc9a46cc4a8579a61ce6c1d62ecd6f15
magicP <42081285+magicP@users.noreply.github.com>
2019-12-02 21:10:10 +0800

    实现一个自定义的ClassLoader (#21)
    实现一个自定义的ClassLoader (#21)

--- src/main/java/com/github/hcsp/classloader/MyClassLoader.java
+++ src/main/java/com/github/hcsp/classloader/MyClassLoader.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.classloader;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class MyClassLoader extends ClassLoader {
     // 存放字节码文件的目录
@@ -25,7 +27,12 @@ public class MyClassLoader extends ClassLoader {
     // 扩展阅读：ClassLoader类的Javadoc文档
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        throw new ClassNotFoundException(name);
+        try {
+            byte[] bytes = Files.readAllBytes(new File(bytecodeFileDirectory, name + ".class").toPath());
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException("IO错误", e);
+        }
     }
 
     public static void main(String[] args) throws Exception {
