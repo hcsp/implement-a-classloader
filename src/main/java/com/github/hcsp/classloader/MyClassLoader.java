169928346874c6097753b7908503575565723ac4
richard1230 <huyongjinnuaa@163.com>
2020-02-21 19:17:42 +0800

    diy-classloader

--- src/main/java/com/github/hcsp/classloader/MyClassLoader.java
+++ src/main/java/com/github/hcsp/classloader/MyClassLoader.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.classloader;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class MyClassLoader extends ClassLoader {
     // 存放字节码文件的目录
@@ -25,8 +27,16 @@ public class MyClassLoader extends ClassLoader {
     // 扩展阅读：ClassLoader类的Javadoc文档
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        throw new ClassNotFoundException(name);
+        byte[] classData = loadClassData(name);
+        return defineClass(name, classData, 0, classData.length);
     }
+
+    private byte[] loadClassData(String className) throws ClassNotFoundException {
+        try {
+            return Files.readAllBytes(new File(className + ".class").toPath());
+        } catch (IOException e) {
+            throw new ClassNotFoundException(className);
+        }    }
 
     public static void main(String[] args) throws Exception {
         File projectRoot = new File(System.getProperty("basedir", System.getProperty("user.dir")));
