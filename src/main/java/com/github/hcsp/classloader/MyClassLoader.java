ef4c80f5bc9b7a50f196ee6678a5f812c8a0b2cb
Harimoto Satoshi <zhanglevan@gmail.com>
2020-04-13 05:38:37 +0800

    Satoshi (#59)
    Satoshi (#59)

--- src/main/java/com/github/hcsp/classloader/MyClassLoader.java
+++ src/main/java/com/github/hcsp/classloader/MyClassLoader.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.classloader;
 
 import java.io.File;
+import java.nio.file.Files;
+
 
 public class MyClassLoader extends ClassLoader {
     // 存放字节码文件的目录
@@ -25,7 +27,12 @@ public class MyClassLoader extends ClassLoader {
     // 扩展阅读：ClassLoader类的Javadoc文档
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        throw new ClassNotFoundException(name);
+        try {
+            byte[] bytes = Files.readAllBytes(bytecodeFileDirectory.toPath().resolve(name + ".class"));
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (Exception e) {
+            throw new ClassNotFoundException();
+        }
     }
 
     public static void main(String[] args) throws Exception {
