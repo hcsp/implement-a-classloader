1a2de6291cf4a32b2d102db8de446ac5a34bced6
McDul <McDul19960103@gmail.com>
2020-07-24 10:54:49 +0800

    实现了一个类加载器

--- src/main/java/com/github/hcsp/classloader/MyClassLoader.java
+++ src/main/java/com/github/hcsp/classloader/MyClassLoader.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.classloader;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class MyClassLoader extends ClassLoader {
     // 存放字节码文件的目录
@@ -25,6 +28,15 @@ public class MyClassLoader extends ClassLoader {
     // 扩展阅读：ClassLoader类的Javadoc文档
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
+        Path path = bytecodeFileDirectory.toPath().resolve(name + ".class");
+        if (Files.exists(path)) {
+            try {
+                byte[] bytes = Files.readAllBytes(path);
+                return defineClass(name, bytes, 0, bytes.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         throw new ClassNotFoundException(name);
     }
 
