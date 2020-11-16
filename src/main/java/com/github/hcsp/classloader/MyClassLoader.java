40f3f1075f1c307b106eec592eaf89680d34f553
biantaiwen <biantaiwen@163.com>
2020-11-16 23:17:41 +0800

    done

--- src/main/java/com/github/hcsp/classloader/MyClassLoader.java
+++ src/main/java/com/github/hcsp/classloader/MyClassLoader.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.classloader;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class MyClassLoader extends ClassLoader {
     // 存放字节码文件的目录
@@ -25,7 +28,12 @@ public class MyClassLoader extends ClassLoader {
     // 扩展阅读：ClassLoader类的Javadoc文档
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        throw new ClassNotFoundException(name);
+        try {
+            byte[] classData = Files.readAllBytes(Paths.get(String.valueOf(bytecodeFileDirectory), name + ".class"));
+            return this.defineClass(name, classData, 0, classData.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException(name);
+        }
     }
 
     public static void main(String[] args) throws Exception {
