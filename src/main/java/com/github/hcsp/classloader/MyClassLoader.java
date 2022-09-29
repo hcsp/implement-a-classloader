5e4ba6ed7d428017cbc8cae525efcb57695bb234
ang tsang <tsang1205@qq.com>
2022-09-29 13:05:19 +0800

    feat: custom classloader (#128)
    feat: custom classloader (#128)

--- src/main/java/com/github/hcsp/classloader/MyClassLoader.java
+++ src/main/java/com/github/hcsp/classloader/MyClassLoader.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.classloader;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class MyClassLoader extends ClassLoader {
     // 存放字节码文件的目录
@@ -25,7 +27,21 @@ public class MyClassLoader extends ClassLoader {
     // 扩展阅读：ClassLoader类的Javadoc文档
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        throw new ClassNotFoundException(name);
+        Class<?> klass = null;
+
+        File classFile = new File(bytecodeFileDirectory, name + ".class");
+        try {
+            byte[] fileContent = Files.readAllBytes(classFile.toPath());
+            klass = defineClass(name, fileContent, 0, fileContent.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (klass == null) {
+            throw new ClassNotFoundException(name);
+
+        }
+        return klass;
     }
 
     public static void main(String[] args) throws Exception {
